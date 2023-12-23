67af96ae45a563b69d120d8f5c37609b1dbc90b6
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-12-22 23:51:32 -0600

    Se agregaron algunas caracterististicas adicionales al login.

--- src/Components/LoginComponent.jsx
+++ src/Components/LoginComponent.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 
 function LoginComponent() {
-  return (
-    <div>
-      <form action=""> 
-      <br />
-        <label htmlFor="">Usuario</label>
-        <input type="text" name="usuario"/>
-        <br />
-        <label htmlFor="">Contraseña</label>
-        <input type="text" name="contrasena" />
-        <br />
-        <button type="submit">Iniciar Sesion</button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h1>Bienvenido al Sistema</h1>
+                
+                <h3>Ingresa tus datos para continuar</h3>
+            </div>
+            <form action="">
+                <br />
+                <label htmlFor="">Usuario</label> 
+                <br />
+                <input type="text" name="usuario" />
+                <br />
+                <label htmlFor="">Contraseña</label>
+                <br />
+                <input type="text" name="contrasena" />
+                <br />
+                <br />
+                <button type="submit">Iniciar Sesion</button>
+            </form>
+        </div>
+    )
 }
 
 export default LoginComponent
