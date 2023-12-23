d644cf2cc6c3fac06b448ce7448e98e0f37ddbe2
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-12-22 23:40:01 -0600

    Se agrego la ruta por defecto de la aplicacion.

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,6 +1,6 @@
 // Root.jsx
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Router } from 'react-router-dom'
 import DashboardComponent from '../Components/DashboardComponent.jsx'
 import LoginComponent from '../Components/LoginComponent.jsx'
 import MaterialsComponent from '../Components/MaterialsComponent.jsx'
@@ -8,6 +8,7 @@ import MaterialsComponent from '../Components/MaterialsComponent.jsx'
 export default function Root() {
     return (
         <Routes>
+            <Route index element={<DashboardComponent />} />
             <Route path="/materials" element={<MaterialsComponent />} />
             <Route path="/login" element={<LoginComponent />} />
             <Route path="/dashboard" element={<DashboardComponent />} />
