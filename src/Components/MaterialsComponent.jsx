80fc3c7805104669badab38dd0813283dab278a1
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-12-23 00:22:38 -0600

    Se agrego una estructura basica para la tabla de materiales.

--- src/Components/MaterialsComponent.jsx
+++ src/Components/MaterialsComponent.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function MaterialsComponent() {
-  return (
-    <div>MaterialsComponent</div>
-  )
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                </tr>
+            </table>
+        </div>
+    )
 }
 
 export default MaterialsComponent
