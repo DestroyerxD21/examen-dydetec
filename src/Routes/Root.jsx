47e86a85c667eaf4c143897570dc9718ae85c4e9
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-12-23 19:55:04 -0600

    Se realizo el respectivo cambio de la ruta DashboardComponent a MainComponent

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,9 +1,10 @@
 // Root.jsx
 import React from 'react'
 import { Route, Routes, Router } from 'react-router-dom'
-import DashboardComponent from '../Components/DashboardComponent.jsx'
+import DashboardComponent from '../Components/MainContentComponent.jsx'
 import LoginComponent from '../Components/LoginComponent.jsx'
 import MaterialsComponent from '../Components/MaterialsComponent.jsx'
+import MainContentComponent from '../Components/MainContentComponent.jsx'
 
 export default function Root() {
     return (
@@ -11,7 +12,7 @@ export default function Root() {
             <Route index element={<LoginComponent />} />
             <Route path="/materials" element={<MaterialsComponent />} />
             <Route path="/login" element={<LoginComponent />} />
-            <Route path="/dashboard" element={<DashboardComponent />} />
+            <Route path="/dashboard" element={<MainContentComponent />} />
         </Routes>
     )
 }
