cc904da97bc9fe40bbb53b93225b2e42ce9e39dd
Valentin Martinez <valentinmartinez1103@gmail.com>
2023-12-23 21:55:38 -0600

    Se agregaron las rutas para la navegacion de los componentes example.

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -5,6 +5,8 @@ import DashboardComponent from '../Components/MainContentComponent.jsx'
 import LoginComponent from '../Components/LoginComponent.jsx'
 import MaterialsComponent from '../Components/MaterialsComponent.jsx'
 import MainContentComponent from '../Components/MainContentComponent.jsx'
+import ExampleComponent1 from '../Components/ExampleComponent1.jsx'
+import ExampleComponent2 from '../Components/ExampleComponent2.jsx'
 
 export default function Root() {
     return (
@@ -13,6 +15,9 @@ export default function Root() {
             <Route path="/materials" element={<MaterialsComponent />} />
             <Route path="/login" element={<LoginComponent />} />
             <Route path="/dashboard" element={<MainContentComponent />} />
+            <Route path="/componentOne" element={<ExampleComponent1 />} />
+            <Route path="/componentTwo" element={<ExampleComponent2 />} />
+            <Route path="/componentTree" element={<ExampleComponent2 />} />
         </Routes>
     )
 }
